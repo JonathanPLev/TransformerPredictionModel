537963eacdcd593c875275a90cf4d018fe871d03
Omkar Kulkarni <omkisaik@gmail.com>
2026-02-09 00:04:19 -0800

    feat(training_dataset): sqlalchemy additional connections

--- src/training_dataset/build_training_dataset.py
+++ src/training_dataset/build_training_dataset.py
@@ -3,9 +3,11 @@ import sqlalchemy as sqla
 # uncomment to connect to the sportsbook database, make sure to ssh tunnel if connecting locally and not from server
 # engine1 = sqla.create_engine("postgresql+psycopg2://line_dancer:sportsbook_data@localhost:5433/nba_deeplearning")
 
-# connect to the nba database - also make sure to ssh tunnel if connecting locally and not from server
-engine2 = sqla.create_engine("postgresql+psycopg2://nba:nba@localhost:5432/nba")
-    
+# to connect straight from server 
+# engine2 = sqla.create_engine("postgresql+psycopg2://nba:nba@localhost:5432/nba")
+
+# to connect from local machine - make sure to ssh tunnel to 55432 (or change connection string gitif you're using a different port)
+engine2 = sqla.create_engine("postgresql+psycopg2://nba:nba@127.0.0.1:55432/nba")
 
 def get_training_data():
     with engine2.connect() as conn:
