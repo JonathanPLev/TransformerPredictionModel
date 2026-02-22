d77b8c2677c092f7dec36ae5dc3aa6dd072061c2
Omkar Kulkarni <omkisaik@gmail.com>
2026-02-22 11:13:45 -0800

    feat(query): time function

--- src/training_player_data/fetch_player_data.py
+++ src/training_player_data/fetch_player_data.py
@@ -1,6 +1,7 @@
 from nba_api.stats.static import players
 import pandas as pd
 import sqlalchemy as sqla
+from datetime import datetime
 
 engine = sqla.create_engine("postgresql+psycopg2://nba:nba@172.24.196.46:5432/nba")
 
@@ -18,17 +19,55 @@ def fetch_player_data(player_name: str):
     player_id = player_found[0]["id"]
 
     with engine.connect() as conn:
-        result = conn.execute(
+        player_data = conn.execute(
             sqla.text("""
-                SELECT *
-                FROM player_statistics
-                WHERE person_id = :pid
-                ORDER BY game_date ASC
+                        SELECT person_id, game_date, points, num_minutes, assists, rebounds_total, turnovers,
+                               field_goals_attempted, three_pointers_attempted, free_throws_attempted, steals,
+                               blocks, plus_minus_points
+                        FROM player_statistics
+                        WHERE person_id = :pid
+                        ORDER BY game_date ASC;
             """),
             {"pid": player_id}
         )
 
-        cols = result.keys()
-        return pd.DataFrame(result.fetchall(), columns=cols)
+        cols = player_data.keys()
+
+        features = pd.DataFrame(player_data.fetchall(), columns=cols)
+
+        player_team = conn.execute(
+            sqla.text("""
+                        SELECT person_id, player_team_name, game_id, game_date
+                        FROM player_statistics
+                        WHERE person_id = :pid
+                        ORDER BY game_date DESC
+                        LIMIT 1;
+            """),
+            {"pid": player_id}
+        )
+
+        # is player currently playing?
+        team_name = player_team.fetchone()[1]
+        
+        # return team_name
+
+        current_time = datetime.now() 
+        
+        # we are grabbing the games that happen after current time
+        game_date = conn.execute(
+            sqla.text("""
+                        SELECT gameid, NULLIF(gamedatetimeest,'')::timestamp AS game_ts_est, hometeamname, awayteamname
+                        FROM games_raw
+                        WHERE (:player_team = hometeamname OR :player_team = awayteamname)
+                        AND NULLIF(gamedatetimeest,'')::timestamp > :time
+                        ORDER BY game_ts_est DESC;
+            """),
+            {"player_team": team_name, "time": current_time}
+        )
+
+
+        return game_date.fetchall()
+
+        # also pull the next opponent game
 
 print(fetch_player_data("LeBron James"))
